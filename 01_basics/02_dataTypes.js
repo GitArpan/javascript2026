d5820c05c04f36edefc0b62c1ff777c675764891
Arpan <arpanbk91@gmail.com>
2026-04-26 15:18:25 +0545

    datatypes learned

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs , not browser
+// react / node // we avoid using semi colon
+// readability is important
+//ex:
+console.log(
+    3+3.      //code readability should be high
+
+)
+
+let name = "Arpan"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//Primitive Datatypes
+// number => 2 to power 53 range
+// bigint
+// string => ""
+// booleaan => true/false
+// null => standalone value
+// undefined =>
+// symbol => useful in react / figma tools / component uniqueness
+
+
+
+// OBJECT : MOST USed
+console.log(typeof "hITESH"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
+
+
