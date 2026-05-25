669793e637210f9a33cfe492f2b230eb044ec461
Arpan <arpanbk91@gmail.com>
2026-05-25 18:31:32 +0545

    learned about block scope and global scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,24 @@
+//var c = 300
+let a = 300
+if (true){
+    let a = 10 
+const b = 20
+ c = 30
+ console.log("INNER: ", a)
+}
+
+
+
+
+
+console.log(a)
+//console.log(b)
+//console.log(c)
+
+// Inside if that is block scpoe
+// outside of if that is global scope
+// The variable in global scope must be available inside the {}
+// The value written inside the scope should not be available outside{}
+
+// there is difference when you check scope in browser and in the code environment , using node , it is a interview question , we will see this next time
+
