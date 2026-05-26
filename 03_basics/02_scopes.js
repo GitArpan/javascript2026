4c65316139ed5595b4d21cd6df43a50e43ffbe42
Arpan <arpanbk91@gmail.com>
2026-05-26 20:40:21 +0545

    scope levwl and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,14 +4,14 @@ if (true){
     let a = 10 
 const b = 20
  c = 30
- console.log("INNER: ", a)
+// console.log("INNER: ", a)
 }
 
 
 
 
 
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)
 
@@ -22,3 +22,49 @@ console.log(a)
 
 // there is difference when you check scope in browser and in the code environment , using node , it is a interview question , we will see this next time
 
+
+
+function one(){
+
+    const username = "hitesh"
+
+    function two (){
+
+        const website = "youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    //two()
+
+
+}
+
+//one()
+
+if (true) {
+    const username ="hitesh"
+    if(username === "hitesh"){
+        const website = "youtube"
+       // console.log("CONCATING",website + username)
+    }
+    //console.log(website)
+}
+
+//console.log(username)
+
+
+
+// +++++++++++++++++++ interesting ++++++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num+1
+
+}
+
+
+// another method to declare function
+addTwo(5)// the function is not gonna execute because its is inside variable
+const addTwo = function(num){
+    return num + 2
+
+}
